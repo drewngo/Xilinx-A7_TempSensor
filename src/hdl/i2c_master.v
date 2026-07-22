54118c1b61f15d306ad8a24ddce11a23ef6fdd97
drewngo <ango3161016@gmail.com>
2026-07-22 15:51:18 -0500

    formatting

--- src/hdl/i2c_master.v
+++ src/hdl/i2c_master.v
@@ -1,237 +1,220 @@
 module i2c_master(
-  input clk,
-  input rst,
-  output [15:0] data,
-  output SCL,
-  inout SDA
+    input         clk,
+    input         rst,
+    output [15:0] data,
+    output        SCL,
+    inout         SDA
 );
-  
-//--------------------------------------------------------------------------
-
-  localparam STATE_IDLE        = 4'd0;
-  localparam STATE_START       = 4'd1;
-  localparam STATE_ADDR_WR     = 4'd2; // ADT7420 ~ 7'h4B , Write ~ 1'b0
-  localparam STATE_ACK_1       = 4'd3;
-  localparam STATE_PTR         = 4'd4;
-  localparam STATE_ACK_2       = 4'd5;
-  localparam STATE_RESTART     = 4'd6;
-  localparam STATE_ADDR_RD     = 4'd7;
-  localparam STATE_ACK_3       = 4'd8;
-  localparam STATE_READ_MSB    = 4'd9;
-  localparam STATE_MASTER_ACK  = 4'd10;
-  localparam STATE_READ_LSB    = 4'd11;
-  localparam STATE_MASTER_NACK = 4'd12;
-  localparam STATE_STOP        = 4'd13;
-  
-//--------------------------------------------------------------------------
-
-  reg [3:0] current_state;
-  reg [3:0] next_state;
-
-//--------------------------------------------------------------------------
-
-  reg [9:0] clk_counter;
-  reg [2:0] addr_bit_counter;
-  reg i2c_clk;
-
-  // strobe ~ essentially a 200kHz timing pulse
-  wire i2c_tick = (clk_counter == 10'd499);
-
-//--------------------------------------------------------------------------
-
-  // 100 kHz clock for i2c
-  always @(posedge clk or negedge rst) begin
     
-    if (!rst) begin
-      clk_counter      <= 10'b0;
-      i2c_clk          <= 1'b1;
-      addr_bit_counter <= 3'd7;
-    end
+//--------------------------------------------------------------------------
 
-    else begin
+    localparam STATE_IDLE        = 4'd0,
+               STATE_START       = 4'd1,
+               STATE_ADDR_WR     = 4'd2, // ADT7420 ~ 7'h4B , Write ~ 1'b0
+               STATE_ACK_1       = 4'd3,
+               STATE_PTR         = 4'd4,
+               STATE_ACK_2       = 4'd5,
+               STATE_RESTART     = 4'd6,
+               STATE_ADDR_RD     = 4'd7,
+               STATE_ACK_3       = 4'd8,
+               STATE_READ_MSB    = 4'd9,
+               STATE_MASTER_ACK  = 4'd10,
+               STATE_READ_LSB    = 4'd11,
+               STATE_MASTER_NACK = 4'd12,
+               STATE_STOP        = 4'd13;
     
-      // i2c clock
-      if (clk_counter == 10'd499) begin
-        clk_counter <= 10'b0;
-        i2c_clk     <= ~i2c_clk;
-      end
-      else
-        clk_counter <= clk_counter + 1'b1;
-      
-      // i2c tick for address bit counter
-      if (i2c_tick) begin
-        case (current_state)
-          STATE_START, 
-          STATE_RESTART, 
-          STATE_ACK_1,
-          STATE_ACK_2,
-          STATE_ACK_3,
-          STATE_MASTER_ACK:
+//--------------------------------------------------------------------------
+
+    reg [3:0] current_state;
+    reg [3:0] next_state;
+
+//--------------------------------------------------------------------------
+
+    reg [9:0] clk_counter;
+    reg [2:0] addr_bit_counter;
+    reg       i2c_clk;
+
+    wire i2c_tick = (clk_counter == 10'd499);
+
+//--------------------------------------------------------------------------
+
+    // 100 kHz clock for i2c
+    always @(posedge clk or negedge rst) begin
+        
+        if (!rst) begin
+            clk_counter      <= 10'b0;
+            i2c_clk          <= 1'b1;
             addr_bit_counter <= 3'd7;
+        end
 
-          STATE_ADDR_WR,
-          STATE_PTR,
-          STATE_ADDR_RD,
-          STATE_READ_MSB,
-          STATE_READ_LSB:
-            addr_bit_counter <= addr_bit_counter - 1'b1;
+        else begin
+            // i2c clock
+            if (clk_counter == 10'd499) begin
+                clk_counter <= 10'b0;
+                i2c_clk     <= ~i2c_clk;
+            end
+            else
+                clk_counter <= clk_counter + 1'b1;
+            
+            // i2c tick for address bit counter
+            if (i2c_tick) begin
+                case (current_state)
+                    STATE_START, 
+                    STATE_RESTART, 
+                    STATE_ACK_1,
+                    STATE_ACK_2,
+                    STATE_ACK_3,
+                    STATE_MASTER_ACK:
+                        addr_bit_counter <= 3'd7;
 
-          default: addr_bit_counter <= addr_bit_counter;
-        endcase
-      end
+                    STATE_ADDR_WR,
+                    STATE_PTR,
+                    STATE_ADDR_RD,
+                    STATE_READ_MSB,
+                    STATE_READ_LSB:
+                        addr_bit_counter <= addr_bit_counter - 1'b1;
+
+                    default: addr_bit_counter <= addr_bit_counter;
+                endcase
+            end
+        end
+
+    end
+
+//--------------------------------------------------------------------------
+
+    // SCL AND SDA BIZ
+    // open drain bus
+    assign SCL = (i2c_clk) ? 1'bz : 1'b0;
+
+    // SDA high on write, SDA low on read (impendance)
+    wire sda_mode = (
+        current_state == STATE_IDLE || 
+        current_state == STATE_START ||
+        current_state == STATE_ADDR_WR ||
+        current_state == STATE_PTR ||
+        current_state == STATE_ADDR_RD ||
+        current_state == STATE_MASTER_ACK
+        ) ? 1'b1 : 1'b0;
     
-    end
+    reg sda_out;
 
-  end
-
-//--------------------------------------------------------------------------
-
-  // SCL AND SDA BIZ
-  // open drain bus
-  assign SCL = (i2c_clk) ? 1'bz : 1'b0;
-
-  // SDA high on write, SDA low on read (impendance)
-  wire sda_mode = (
-    current_state == STATE_IDLE || 
-    current_state == STATE_START ||
-    current_state == STATE_ADDR_WR ||
-    current_state == STATE_PTR ||
-    current_state == STATE_ADDR_RD ||
-    current_state == STATE_MASTER_ACK
-    ) ? 1'b1 : 1'b0;
-  
-  reg sda_out;
-
-  // tri-state buffer
-  assign SDA = (sda_mode) ? ((sda_out) ? 1'bz : 1'b0) : 1'bz;
+    // tri-state buffer
+    assign SDA = (sda_mode) ? ((sda_out) ? 1'bz : 1'b0) : 1'bz;
 
 //--------------------------------------------------------------------------
 
-  // state register
-  always @(posedge clk or negedge rst) begin
-    if (!rst) begin
-      current_state <= STATE_IDLE;
+    // state register
+    always @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            current_state <= STATE_IDLE;
+        end
+        else if (i2c_tick) begin
+            current_state <= next_state;
+        end
     end
-    else if (i2c_tick) begin
-      current_state <= next_state;
-    end
-  end
 
 //--------------------------------------------------------------------------
 
-  // state traversal
-  always @(*) begin
-    next_state = current_state;
+    // state traversal
+    always @(*) begin
+        next_state = current_state;
 
-    case (current_state)
-      STATE_IDLE: begin
-        next_state = STATE_START;
-      end
-
-      STATE_START: begin
-        next_state = STATE_ADDR_WR;
-      end
-
-      STATE_ADDR_WR: begin
-        if (i2c_tick && addr_bit_counter == 3'd0) begin
-          next_state = STATE_ACK_1;
-        end
-        else begin
-          next_state = STATE_ADDR_WR;
-        end
-      end
-
-      STATE_ACK_1: begin
-        if (i2c_tick && i2c_clk) begin
-          next_state = STATE_PTR;
-        end
-        else begin
-          next_state = STATE_ACK_1;
-        end
-      end
-
-      STATE_PTR: begin
-        if (i2c_tick && addr_bit_counter == 3'd0) begin
-          next_state = STATE_ACK_2;
-        end
-        else begin
-          next_state = STATE_PTR;
-        end
-      end
-
-      STATE_ACK_2: begin
-        if (i2c_tick && i2c_clk) begin
-          next_state = STATE_RESTART;
-        end
-        else begin
-          next_state = STATE_ACK_2;
-        end
-      end
-
-      STATE_RESTART: begin
-        next_state = STATE_ADDR_RD;
-      end
-
-      STATE_ADDR_RD: begin
-        if (i2c_tick && addr_bit_counter == 3'd0) begin
-          next_state = STATE_ACK_3;
-        end
-        else begin
-          next_state = STATE_ADDR_RD;
-        end
-      end
-
-      STATE_ACK_3: begin
-        if (i2c_tick && i2c_clk) begin
-          next_state = STATE_READ_MSB;
-        end
-        else begin
-          next_state = STATE_ACK_3;
-        end
-      end
-      
-      STATE_READ_MSB: begin
-        if (i2c_tick && addr_bit_counter == 3'd0) begin
-          next_state = STATE_MASTER_ACK;
-        end
-        else begin
-          next_state = STATE_READ_MSB;
-        end
-      end
-
-      STATE_MASTER_ACK: begin
-        if (i2c_tick && i2c_clk) begin
-          next_state = STATE_READ_LSB;
-        end
-        else begin
-          next_state = STATE_MASTER_ACK;
-        end
-      end
-
-      STATE_READ_LSB: begin
-        if (i2c_tick && addr_bit_counter == 3'd0) begin
-          next_state = STATE_MASTER_NACK;
-        end
-        else begin
-          next_state = STATE_READ_LSB;
-        end
-      end
-
-      STATE_MASTER_NACK: begin
-        if (i2c_tick && i2c_clk) begin
-          next_state = STATE_STOP;
-        end
-        else begin
-          next_state = STATE_MASTER_NACK;
-        end
-      end
-
-      STATE_STOP: begin
-        next_state = STATE_IDLE;
-      end
-
-    endcase
-  end
+        case (current_state)
+            STATE_IDLE        : begin
+                next_state = STATE_START;
+            end
+            STATE_START       : begin
+                next_state = STATE_ADDR_WR;
+            end
+            STATE_ADDR_WR     : begin
+                if (i2c_tick && addr_bit_counter == 3'd0) begin
+                    next_state = STATE_ACK_1;
+                end
+                else begin
+                    next_state = STATE_ADDR_WR;
+                end
+            end
+            STATE_ACK_1       : begin
+                if (i2c_tick && i2c_clk) begin
+                    next_state = STATE_PTR;
+                end
+                else begin
+                    next_state = STATE_ACK_1;
+                end
+            end
+            STATE_PTR         : begin
+                if (i2c_tick && addr_bit_counter == 3'd0) begin
+                    next_state = STATE_ACK_2;
+                end
+                else begin
+                    next_state = STATE_PTR;
+                end
+            end
+            STATE_ACK_2       : begin
+                if (i2c_tick && i2c_clk) begin
+                    next_state = STATE_RESTART;
+                end
+                else begin
+                    next_state = STATE_ACK_2;
+                end
+            end
+            STATE_RESTART     : begin
+                next_state = STATE_ADDR_RD;
+            end
+            STATE_ADDR_RD     : begin
+                if (i2c_tick && addr_bit_counter == 3'd0) begin
+                    next_state = STATE_ACK_3;
+                end
+                else begin
+                    next_state = STATE_ADDR_RD;
+                end
+            end
+            STATE_ACK_3       : begin
+                if (i2c_tick && i2c_clk) begin
+                    next_state = STATE_READ_MSB;
+                end
+                else begin
+                    next_state = STATE_ACK_3;
+                end
+            end
+            STATE_READ_MSB    : begin
+                if (i2c_tick && addr_bit_counter == 3'd0) begin
+                    next_state = STATE_MASTER_ACK;
+                end
+                else begin
+                    next_state = STATE_READ_MSB;
+                end
+            end
+            STATE_MASTER_ACK  : begin
+                if (i2c_tick && i2c_clk) begin
+                    next_state = STATE_READ_LSB;
+                end
+                else begin
+                    next_state = STATE_MASTER_ACK;
+                end
+            end
+            STATE_READ_LSB    : begin
+                if (i2c_tick && addr_bit_counter == 3'd0) begin
+                    next_state = STATE_MASTER_NACK;
+                end
+                else begin
+                    next_state = STATE_READ_LSB;
+                end
+            end
+            STATE_MASTER_NACK : begin
+                if (i2c_tick && i2c_clk) begin
+                    next_state = STATE_STOP;
+                end
+                else begin
+                    next_state = STATE_MASTER_NACK;
+                end
+            end
+            STATE_STOP        : begin
+                next_state = STATE_IDLE;
+            end
+        endcase
+    end
 
 //--------------------------------------------------------------------------
 
--- src/hdl/seven_seg_driver.v
+++ src/hdl/seven_seg_driver.v
@@ -1,74 +1,78 @@
 module seven_seg_driver(
-  input clk,
-  input rst,
-  input [15:0] data_in,
-  output reg [7:0] an,
-  output reg [6:0] seg
+    input             clk,
+    input             rst,
+    input      [15:0] data_in,
+    output reg [ 7:0] an,
+    output reg [ 6:0] seg
 );
 
-  // ------
-  reg [15:0] clk_div;      // 100 MHz clock from Nexys A7 -> 1525.8Hz
-  reg [1:0] digit_select;
-  reg [3:0] BCD_digit;
-  
-  // ------
-  always @(posedge clk or negedge rst) begin
+//--------------------------------------------------------------------------
 
-    if (!rst) begin
-      clk_div       <= 16'd0;
-      digit_select <= 2'b00;
+    reg [15:0] clk_div;          // 100 MHz clock from Nexys A7 -> 1525.8Hz
+    reg [ 1:0] digit_select;
+    reg [ 3:0] BCD_digit;
+    
+//--------------------------------------------------------------------------
 
-    end else begin
-      clk_div <= clk_div + 1'b1;
-      
-      if (clk_div == 16'hFFFF) begin
-        digit_select <= digit_select + 1'b1;
-      end
+    always @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            clk_div      <= 16'd0;
+            digit_select <= 2'b00;
+
+        end else begin
+            clk_div <= clk_div + 1'b1;
+            
+            if (clk_div == 16'hFFFF) begin
+                digit_select <= digit_select + 1'b1;
+            end
+        end
+    end
+
+//--------------------------------------------------------------------------
+
+    always @(*) begin
+        an = 8'b1111_1111;
+        BCD_digit = 4'd0;
+        
+        case (digit_select)
+            2'b00: begin
+                an = 8'b1111_1110;
+                BCD_digit = data_in[ 3: 0];
+            end
+            2'b01: begin
+                an = 8'b1111_1101;
+                BCD_digit = data_in[ 7: 4];
+            end
+            2'b10: begin
+                an = 8'b1111_1011;
+                BCD_digit = data_in[11: 8];
+            end
+            2'b11: begin
+                an = 8'b1111_0111;
+                BCD_digit = data_in[15:12];
+            end
+        endcase
     end
     
-  end
-  
-  // ------
-  always @(*) begin
-    an = 8'b1111_1111;
-    BCD_digit = 4'd0;
+//--------------------------------------------------------------------------
+
+    always @(*) begin
+        seg = 7'b111_1111;
     
-    case (digit_select)
-      2'b00: begin
-        an = 8'b1111_1110;
-        BCD_digit = data_in[3:0];
-      end
-      2'b01: begin
-        an = 8'b1111_1101;
-        BCD_digit = data_in[7:4];
-      end
-      2'b10: begin
-        an = 8'b1111_1011;
-        BCD_digit = data_in[11:8];
-      end
-      2'b11: begin
-        an = 8'b1111_0111;
-        BCD_digit = data_in[15:12];
-      end
-    endcase
-  end
-  
-  // ------
-  always @(*) begin
-    seg = 7'b111_1111;
-  
-    case (BCD_digit)
-      4'd0:    seg = 7'b000_0001;
-      4'd1:    seg = 7'b100_1111;
-      4'd2:    seg = 7'b001_0010;
-      4'd3:    seg = 7'b000_0110;
-      4'd4:    seg = 7'b100_1100;
-      4'd5:    seg = 7'b010_0100;
-      4'd6:    seg = 7'b010_0000;
-      4'd7:    seg = 7'b000_1111;
-      4'd8:    seg = 7'b000_0000;
-      4'd9:    seg = 7'b000_1100;
-    endcase
-  end
-  
+        case (BCD_digit)
+            4'd0:        seg = 7'b000_0001;
+            4'd1:        seg = 7'b100_1111;
+            4'd2:        seg = 7'b001_0010;
+            4'd3:        seg = 7'b000_0110;
+            4'd4:        seg = 7'b100_1100;
+            4'd5:        seg = 7'b010_0100;
+            4'd6:        seg = 7'b010_0000;
+            4'd7:        seg = 7'b000_1111;
+            4'd8:        seg = 7'b000_0000;
+            4'd9:        seg = 7'b000_1100;
+        endcase
+    end
+    
+//--------------------------------------------------------------------------
+
 endmodule
